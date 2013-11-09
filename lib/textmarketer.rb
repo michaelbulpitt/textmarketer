7b9caf726fff139dc55e2359dcba7276983dfb23
Michael Bulpitt <michael@kashoo.co.uk>
2013-11-09 17:38:44 +0000

    implemented send def

--- lib/textmarketer.rb
+++ lib/textmarketer.rb
@@ -1,5 +1,16 @@
 require "textmarketer/version"
+require 'net/http'
+require 'uri'
 
 module Textmarketer
-  # Your code goes here...
+  def self.send username, password, to, message, from
+  	requested_url = 'http://api.textmarketer.co.uk/gateway/?option=xml' + "&username=" + username + "&password=" + password + "&to=" + to + "&message=" + URI.escape(message) + "&orig=" + URI.escape(from)
+	  url = URI.parse(requested_url)
+	  full_path = (url.query.blank?) ? url.path : "#{url.path}?#{url.query}"
+	  the_request = Net::HTTP::Get.new(full_path)
+	  the_response = Net::HTTP.start(url.host, url.port) { |http|
+	    http.request(the_request)
+	  }
+	  raise "Response was not 200, response was #{the_response.code}" if the_response.code != "200"
+  end
 end
